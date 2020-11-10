39a954fbac84c1fc1aa85455a259ec5a28cba79e
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-10 15:43:01 -0500

    finishing seeing how the head moves in new branch

--- src/TestingBranches_featureA.java
+++ src/TestingBranches_featureA.java
@@ -2,5 +2,6 @@ public class TestingBranches_featureA {
     public static void main(String[] args) {
         System.out.println("Feature-a branch");
         //adding comment to see the movement of the head in the Git log
+        //line 3
     }
 }
