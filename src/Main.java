e656d744bc3aabf125fdbaf7f6774276d23128a7
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-10 15:54:26 -0500

    finishing seeing how the head moves in new branch

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,12 @@ public class Main {
         System.out.println("Jimmy");
         //added a comment
         //added another comment
+        /*
+        branching in Git is like opening up a new timeline for new work to not affect whats already in master
+        branch. eventually you can merge them if the new code works. If the code doesnt work then you can just delete
+        that timeline
+
+         */
+
     }
 }
