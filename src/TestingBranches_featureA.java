2b8d21488adde69a11a076f856bf5739bab2b9a6
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-10 15:42:23 -0500

    following the head in Git log

--- src/TestingBranches_featureA.java
+++ src/TestingBranches_featureA.java
@@ -1,5 +1,6 @@
 public class TestingBranches_featureA {
     public static void main(String[] args) {
         System.out.println("Feature-a branch");
+        //adding comment to see the movement of the head in the Git log
     }
 }
