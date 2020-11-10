ae2e9a8b91f94056ddd009d681c99fb24b4e0891
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-10 15:45:24 -0500

    finishing seeing how the head moves in new branch

--- src/TestingBranches_featureA.java
+++ src/TestingBranches_featureA.java
@@ -3,5 +3,11 @@ public class TestingBranches_featureA {
         System.out.println("Feature-a branch");
         //adding comment to see the movement of the head in the Git log
         //line 3
+        /*
+        branching in Git is like opening up a new timeline for new work to not affect whats already in master
+        branch. eventually you can merge them if the new code works. If the code doesnt work then you can just delete
+        that timeline
+         */
+
     }
 }
