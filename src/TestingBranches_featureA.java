e690d369af586e703cc27089291cbb2104518ecc
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-10 15:53:39 -0500

    finishing seeing how the head moves in new branch

--- src/TestingBranches_featureA.java
+++ src/TestingBranches_featureA.java
@@ -7,6 +7,7 @@ public class TestingBranches_featureA {
         branching in Git is like opening up a new timeline for new work to not affect whats already in master
         branch. eventually you can merge them if the new code works. If the code doesnt work then you can just delete
         that timeline
+
          */
 
     }
